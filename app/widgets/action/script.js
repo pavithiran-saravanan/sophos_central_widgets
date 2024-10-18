8018ca5cb91c12752ebe1819e41928f03d10dd94
ranjith-15557 <ranjith.pr@zohocorp.com>
2024-10-18 14:06:10 +0530

    feat: add isolate and delete endpoint action

--- app/widgets/action/script.js
+++ app/widgets/action/script.js
@@ -1,3 +1,54 @@
+async function isolateEndpoint() {
+  const queryParams = new URLSearchParams();
+  queryParams.append('id', '6f56511d-f3f9-4c39-9b7b-113cc6a638fd');
+
+  const isolationStatusResponse = await fetch(`/endpoints/isolate?${queryParams}`);
+  const isolationStatusData = await isolationStatusResponse.json();
+  if(isolationStatusResponse.ok){
+    if(isolationStatusData['enabled']){
+      const status = document.getElementById("isolateStatus");
+      status.innerText = "Endpoint Already Isolated";
+    } else {
+      await fetch("/endpoints/isolate", {
+        method: "PATCH",
+        headers: {
+          Accept: "application/json",
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          id
+        }),
+      }).then(async (res) => {
+        const data = await res.json();
+        console.log(data);
+        if (res.ok && data.response === "success") {
+          const status = document.getElementById("isolateStatus");
+          status.innerText = "Isolated Successfully";
+        }
+      });
+    }
+  }
+}
+
+async function deleteEndpoint() {
+  const queryParams = new URLSearchParams();
+  queryParams.append('id', '6f56511d-f3f9-4c39-9b7b-113cc6a638fd');
+
+  await fetch(`/endpoints?${queryParams}`, {
+    method: "DELETE",
+    headers: {
+      Accept: "application/json",
+    },
+  }).then(async (res) => {
+    const data = await res.json();
+    console.log(data);
+    if (res.ok && data.response === "success") {
+      const status = document.getElementById("deleteStatus");
+      status.innerText = "Deleted Successfully";
+    }
+  });
+}
+
 async function scanEndpoint() {
   const endpointID = "6f56511d-f3f9-4c39-9b7b-113cc6a638fd";
 
--- server/js/sophosAPI/endpointAPI.js
+++ server/js/sophosAPI/endpointAPI.js
@@ -24,6 +24,42 @@ router.get("/:endpointID", async function (req, res) {
   }
 });
 
+router.delete("/", async function (req, res) {
+  try {
+    const { id } = req.query;
+    const data = await deleteEndpoint(id);
+    console.log("data ", data);
+    res.send(data);
+  } catch (err) {
+    console.log(err);
+    res.json({ error: "something not correct" });
+  }
+});
+
+router.get("/isolate", async (req, res) => {
+  try {
+    const { id } = req.query;
+    const data = await getEndpointIsolation(id);
+    console.log("data ", data);
+    res.send(data);
+  } catch (err) {
+    console.log(err);
+    res.json({ error: "something not correct" });
+  }
+});
+
+router.patch("/isolate", async (req, res) => {
+  try {
+    const { id } = req.body;
+    const data = await isolateEndpoint(id);
+    console.log("data ", data);
+    res.send(data);
+  } catch (err) {
+    console.log(err);
+    res.json({ error: "something not correct" });
+  }
+});
+
 async function getEndpoints() {
   const res = await fetch(
     `https://api-${authProp.dataRegion}.central.sophos.com/endpoint/v1/endpoints`,
@@ -70,6 +106,95 @@ async function getEndpointDetails(endpointID) {
     if (!response.ok) {
       throw new Error("Network response was not ok " + response.statusText);
     }
+    return data;
+  });
+  return res;
+}
+
+async function deleteEndpoint(endpointID) {
+  const res = await fetch(
+    `https://api-${authProp.dataRegion}.central.sophos.com/endpoint/v1/endpoints/${endpointID}`,
+    {
+      method: "DELETE",
+      headers: {
+        Authorization: `Bearer ${authProp.access_token}`,
+        "X-Tenant-ID": authProp.tenantID,
+        Accept: "application/json",
+      }
+    }
+  ).then(async (res) => {
+    const data = await res.json();
+    console.log("response data", data);
+    if (res.status === 401) {
+      await Authenticate();
+      console.log("new access_token", authProp.access_token);
+      deleteEndpoint(endpointID)
+    }
+    if (!res.ok) {
+      throw new Error("Network response was not ok " + res.statusText);
+    }
+    data.response = "success";
+    return data;
+  });
+  return res;
+}
+
+async function getEndpointIsolation(endpointID) {
+  const res = await fetch(
+    `https://api-${authProp.dataRegion}.central.sophos.com/endpoint/v1/endpoints/${endpointID}/isolation`,
+    {
+      method: "GET",
+      headers: {
+        Authorization: `Bearer ${authProp.access_token}`,
+        "X-Tenant-ID": authProp.tenantID,
+        Accept: "application/json"
+      }
+    }
+  ).then(async (res) => {
+    const data = await res.json();
+    console.log("response data", data);
+    if (res.status === 401) {
+      await Authenticate();
+      console.log("new access_token", authProp.access_token);
+      getEndpointIsolation(endpointID)
+    }
+    if (!res.ok) {
+      throw new Error("Network response was not ok " + res.statusText);
+    }
+    data.response = "success";
+    return data;
+  });
+  return res;
+}
+
+async function isolateEndpoint(endpointID) {
+  const res = await fetch(
+    `https://api-${authProp.dataRegion}.central.sophos.com/endpoint/v1/endpoints/${endpointID}/isolation`,
+    {
+      method: "PATCH",
+      headers: {
+        Authorization: `Bearer ${authProp.access_token}`,
+        "X-Tenant-ID": authProp.tenantID,
+        Accept: "application/json",
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        "enabled": true,
+        "comment": "Isolate - Log360 Cloud"
+      }),
+    }
+  ).then(async (res) => {
+    const data = await res.json();
+    console.log("response data", data);
+    if (res.status === 401) {
+      await Authenticate();
+      console.log("new access_token", authProp.access_token);
+      isolateEndpoint(endpointID)
+    }
+    if (!res.ok) {
+      throw new Error("Network response was not ok " + res.statusText);
+    }
+    data.response = "success";
     return data;
   });
   return res;
